ee9fc0fc8a79404546c9258d7170e9794798439e
Austin Szema <aszema13@gmail.com>
2023-04-10 20:10:20 -0400

    Built out sample level
    Look at Enemy Test Scene Do Not Use

--- Assets/EnemyShoot.cs
+++ Assets/EnemyShoot.cs
@@ -6,32 +6,40 @@ public class EnemyShoot : MonoBehaviour
 {
 
     [SerializeField] private GameObject bulletPrefab;
+
     [SerializeField] private Transform gunTransform;
 
     [SerializeField] private Rigidbody2D gunRB;
 
     [SerializeField] private Rigidbody2D enemyRB;
 
-
     [SerializeField] private Rigidbody2D playerRB;
-
 
     [SerializeField] private GameObject shootingPoint;
 
+    [SerializeField] private SpriteRenderer gunSpriteRenderer;
+
+
+    [Header("Gun Stuff")]
 
     [SerializeField] private float bulletSpeed;
+    [SerializeField] private float bulletUpOffset;
+    [SerializeField] private float bulletDownOffset;
 
-    [SerializeField] private SpriteRenderer gunSpriteRenderer;
+
+    [SerializeField] private float shootingStartDelay;
+
+    [SerializeField] private float delayBetweenShots;
+
+
 
     private Vector2 lookDir;
     private float angle;
 
 
-
     void Start()
     {
-
-        InvokeRepeating("Shoot", 2.0f, .5f);
+        InvokeRepeating("Shoot", shootingStartDelay, delayBetweenShots);
     }
 
 
@@ -41,7 +49,7 @@ public class EnemyShoot : MonoBehaviour
 
         Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
 
-        bulletRB.velocity = shootingPoint.transform.right * bulletSpeed;
+        bulletRB.velocity = shootingPoint.transform.right * bulletSpeed + shootingPoint.transform.up * Random.Range(bulletDownOffset, bulletUpOffset);
 
     }
 
@@ -62,7 +70,7 @@ public class EnemyShoot : MonoBehaviour
 
     private void Update()
     {
-        if(playerRB.position.x > enemyRB.position.x)
+        if (playerRB.position.x > enemyRB.position.x)
         {
             flipYFalse();
 
@@ -82,8 +90,8 @@ public class EnemyShoot : MonoBehaviour
         lookDir = playerRB.position - gunRB.position;
 
         angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        gunRB.rotation = angle;
 
+        gunRB.rotation = angle;
 
     }
 
