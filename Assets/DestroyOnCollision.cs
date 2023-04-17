1bb9117f677e4adb35061350004e0a4b01a8129d
Austin Szema <aszema13@gmail.com>
2023-04-17 18:48:14 -0400

    Changed Bullet Prefab to use trigger instead of collider

--- Assets/DestroyOnCollision.cs
+++ Assets/DestroyOnCollision.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+     
         Destroy(gameObject);
     }
 }
